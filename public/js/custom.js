d4610c016c763f33ed0497dc97ff8cde6b4f23f1
Chris Milliano <MillianoC@gmail.com>
2016-09-30 23:02:42 -0500

    Initial commit

---
+++ public/js/custom.js
@@ -0,0 +1,33 @@
+// Handle visibility of "back-to-top" button
+window.onscroll = function() {
+    if ($(document).scrollTop() > 140) {
+        showBackToTop();
+    }
+    else {
+        hideBackToTop();
+    }
+};
+
+
+// Displays "back-to-top" button
+var showBackToTop = function() {
+    $('#back-to-top').removeClass("hidden");
+}
+
+
+// Hides "back-to-top" button
+var hideBackToTop = function() {
+    $('#back-to-top').addClass("hidden");
+}
+
+
+// Sets window location on clicking "back-to-top"
+var backToTop = function() {
+    window.scrollTo(0,0);
+}
+
+
+// Toggles "sticky-navbar-content" open and closed
+var toggleStickyNav = function() {
+    $('#sticky-navbar-content').toggleClass("hidden");
+}
